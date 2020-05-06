ed5a95966721bc3084f986cc2317a3b9828d1513
Frederico Moreira <frederico.moreira@vision-box.com>
2020-05-06 20:14:47 +0100

    Included resolvers empty and basic schema

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import { GraphQLServer } from 'graphql-yoga';
+import { resolve } from 'path';
+import resolvers from './resolvers/resolvers';
+
+const server = new GraphQLServer({
+    typeDefs: resolve(__dirname, 'schema/schema.graphql'),
+    resolvers
+});
+
+server.start();
