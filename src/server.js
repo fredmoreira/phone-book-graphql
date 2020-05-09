577acde9c88e6a2924b9be4152b7195b1ff3dc27
Frederico Moreira <fredmoreira@Fredericos-MBP.lan>
2020-05-09 16:12:19 +0100

    Included mongoose, models e mongo db connection

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 import { GraphQLServer } from 'graphql-yoga';
+import mongoose from 'mongoose';
 import { resolve } from 'path';
 import resolvers from './resolvers/resolvers';
+
+mongoose.connect('mongodb://localhost:27017/phone-book-graphql', {
+  useNewUrlParser: true,
+});
 
 const server = new GraphQLServer({
   typeDefs: resolve(__dirname, 'schema/schema.graphql'),
